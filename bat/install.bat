16d4a551bb7efcc09703ce93432fc8b11ce82b7b
PsychedelicsEyes <eyespsychedelics@gmail.com>
2021-08-27 14:46:08 +0200

    Slt cv?

---
+++ bat/install.bat
@@ -0,0 +1,8 @@
+@echo off
+
+title Leafta@Bot
+
+echo Installation des modules
+:START
+npm install
+goto START
---
+++ bat/run.bat
@@ -0,0 +1,10 @@
+@echo off
+
+title Leafta@Bot
+
+echo Voulez vous lancer le  bot?
+pause
+
+:START
+node index.js
+goto START
